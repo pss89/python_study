41478f78912e80d0868bdc2ec21d89961b6dd45c
pss89 <sosn3@naver.com>
2021-11-23 23:47:35 +0900

    파이썬 공부 3일차 추가

--- django/nc_section_py/python_35.py
+++ django/nc_section_py/python_35.py
@@ -1,16 +1,4 @@
-# 예외처리 안했을 때
-# print("나누기 전용 계산기 입니다.")
-# nums = []
-# nums.append(int(input("첫 번째 숫자를 입력하세요 : ")))
-# nums.append(int(input("두 번째 숫자를 입력하세요 : ")))
-# nums.append(int(nums[0] / nums[1]))
-# print("{0} / {1} = {2}".format(nums[0],nums[1],nums[2]))
-
-# num1 = int(input("첫 번째 숫자를 입력하세요 : "))
-# num2 = int(input("두 번째 숫자를 입력하세요 : "))
-# print("{0} / {1} = {2}".format(num1,num2,int(num1/num2)))
-
-# 예외처리 했을 때
+# 예외처리
 try:
     print("나누기 전용 계산기 입니다.")
     nums = []
