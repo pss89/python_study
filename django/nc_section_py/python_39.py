cd8893933bbe08971976d57680d878d0e8184af1
pss89 <sosn3@naver.com>
2021-11-25 00:23:22 +0900

    파이썬 공부 4일차

---
+++ django/nc_section_py/python_39.py
@@ -0,0 +1,24 @@
+# 모듈
+# import theater_module
+# theater_module.price(3) # 3명 영화
+# theater_module.price_morning(4) # 4명 조조영화
+# theater_module.price_soldier(5) # 5명 군인할인
+
+# import theater_module as mv
+# mv.price(3)
+# mv.price_morning(4)
+# mv.price_soldier(5)
+
+# from 모두 사용하겠다 , import 특정 모듈만 사용하겠다
+# as 별칭
+# from theater_module import *
+# price(3)
+# price_morning(4)
+# price_soldier(5)
+
+# from theater_module import price, price_morning
+# print(5)
+# price_morning(6)
+
+# from theater_module import price_soldier as ps
+# ps(5)
--- django/practice.py
+++ django/practice.py
@@ -1,31 +1,9 @@
-# 예외처리 안했을 때
-# print("나누기 전용 계산기 입니다.")
-# nums = []
-# nums.append(int(input("첫 번째 숫자를 입력하세요 : ")))
-# nums.append(int(input("두 번째 숫자를 입력하세요 : ")))
-# nums.append(int(nums[0] / nums[1]))
-# print("{0} / {1} = {2}".format(nums[0],nums[1],nums[2]))
+def solution(s):
+    if s.isdigit() == True and (len(s) == 4 or len(s) == 6):
+        answer = True
+    else:
+        answer = False
+    return answer
 
-# num1 = int(input("첫 번째 숫자를 입력하세요 : "))
-# num2 = int(input("두 번째 숫자를 입력하세요 : "))
-# print("{0} / {1} = {2}".format(num1,num2,int(num1/num2)))
-
-# 예외처리 했을 때
-try:
-    print("나누기 전용 계산기 입니다.")
-    nums = []
-    nums.append(int(input("첫 번째 숫자를 입력하세요 : ")))
-    nums.append(int(input("두 번째 숫자를 입력하세요 : ")))
-    # nums.append(int(nums[0] / nums[1]))
-    print("{0} / {1} = {2}".format(nums[0],nums[1],nums[2]))
-    # num1 = int(input("첫 번째 숫자를 입력하세요 : "))
-    # num2 = int(input("두 번째 숫자를 입력하세요 : "))
-    # print("{0} / {1} = {2}".format(num1,num2,int(num1/num2)))
-except ValueError:
-    print("에러! 잘못된 값을 입력하였습니다")
-except ZeroDivisionError as err:
-    print(err)
-# except:
-except Exception as err:
-    print(err)
-    # print("알 수 없는 에러가 발생하였습니다")
+s = 'a234'
+print(solution(s))
